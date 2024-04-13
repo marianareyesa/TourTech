fb3c7c917f993e61295e27bac9173a348861f14e
KarimGhazzi <kelghazzi.ieu2021@student.ie.edu>
2024-04-13 18:49:25 +0200

    specific pdf pages fix for eurotrip pdfs

--- rag.py
+++ rag.py
@@ -8,6 +8,37 @@ from langchain.memory import ConversationBufferMemory
 from PyPDF2 import PdfReader
 import os
 
+specific_pages = [3]
+
+def extract_and_process_pdf_long(pdf_file_paths, specific_pages):
+    all_data = []
+    for pdf_file_path in pdf_file_paths:
+        # Extract text from the PDF from specified pages only
+        pdf_text = ""
+        with open(pdf_file_path, 'rb') as f:
+            pdf_reader = PdfReader(f)
+            for page_number in specific_pages:
+                try:
+                    page = pdf_reader.pages[page_number - 1]  # Adjust for zero-based index
+                    pdf_text += page.extract_text() or ""
+                except IndexError:
+                    print(f"Page {page_number} does not exist in {pdf_file_path}.")
+
+        # Create a temporary text file
+        temp_txt_filename = pdf_file_path.replace('.pdf', '.txt')
+        with open(temp_txt_filename, 'w', encoding='utf-8') as f:
+            f.write(pdf_text)
+
+        # Use the extracted text file for further processing
+        loader = TextLoader(file_path=temp_txt_filename, encoding="utf-8")
+        data = loader.load()
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        data = text_splitter.split_documents(data)
+        all_data.extend(data)
+    
+    return all_data
+
+
 def extract_and_process_pdf(pdf_file_paths):
     all_data = []
     for pdf_file_path in pdf_file_paths:
@@ -56,16 +87,27 @@ api_key = "api-key"
 
 # Define the list of PDF files
 pdf_files = [
-    "/Users/mariana/Desktop/stats/TourTech/trip_plans/italia.pdf",
-    "/Users/mariana/Desktop/stats/TourTech/trip_plans/coracia_bosnia.pdf",
-    "/Users/mariana/Desktop/stats/TourTech/trip_plans/grecia.pdf"
+    "trip_plans/italia.pdf",
+    "trip_plans/coracia_bosnia.pdf",
+    "trip_plans/grecia.pdf"
+]
+
+pdf_files_long = [
+    "trip_plans/eurotrip.pdf"
 ]
 
 # Define the query
 query = "Que paises estan siendo mencionados?"
 
 # Extract and process PDF text for all documents
+processed_texts_long = extract_and_process_pdf_long(pdf_files_long, specific_pages)
 processed_texts = extract_and_process_pdf(pdf_files)
+
+# Group the processed texts and processed texts long
+processed_texts = processed_texts + processed_texts_long
+
+
+
 
 # Create conversation chain with combined processed text
 conversation_chain = create_conversation_chain(processed_texts, api_key)
